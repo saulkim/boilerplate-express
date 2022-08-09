76552e575ca4aee0fb17746f519ab889c40f5bf7
saulkim <>
2022-08-08 21:50:05 -0700

    added json

--- myApp.js
+++ myApp.js
@@ -10,7 +10,11 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
+app.get("/json", function(req, res){
+    res.json({
+	"message": "Hello json"
+    });
+});
 
 
 
